fed1cc90ab460aa2ffa2e356669c29b9f1c2d7e8
jjomathew98 <141969022+jjomathew98@users.noreply.github.com>
2023-12-16 15:55:44 -0500

    Create populate.sql

---
+++ mysqlquery/populate.sql
@@ -0,0 +1,9 @@
+INSERT INTO patients (first_name, last_name, date_of_birth) VALUES 
+('Sam', 'Mathew', '1921-10-15'),
+('John', 'Hann', '1911-10-17'),
+('aaron', 'jojo', '1931-10-20');
+
+INSERT INTO medications (medication_name) VALUES
+('ibuprofen'), 
+('tylenol'), 
+('Flonase');
